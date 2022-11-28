744857858d73b2decfb8d873e70dbc6f4614429d
Andrew Wyllie <a.wyllie2@gmail.com>
2022-11-28 12:30:05 +0000

    Bind 2015/2016, discard empty columns

--- task4/data_cleaning_scripts/clean_data.R
+++ task4/data_cleaning_scripts/clean_data.R
@@ -3,15 +3,14 @@ library(readxl)
 library(here)
 library(janitor)
 
-what <- read_xlsx(here("raw_data/boing-boing-candy-2015.xlsx")) %>% 
+data_2015 <- read_xlsx(here("raw_data/boing-boing-candy-2015.xlsx")) %>% 
   clean_names()
-is_even <- read_xlsx(here("raw_data/boing-boing-candy-2016.xlsx")) %>% 
+data_2016 <- read_xlsx(here("raw_data/boing-boing-candy-2016.xlsx")) %>% 
   clean_names()
-happening <- read_xlsx(here("raw_data/boing-boing-candy-2017.xlsx")) %>% 
+data_2017 <- read_xlsx(here("raw_data/boing-boing-candy-2017.xlsx")) %>% 
   clean_names()
 
-what_names <- colnames(what)
-is_even_names <- colnames(is_even)
-happening_names <- colnames(happening)
+bound_table_1516 <- data_2015 %>% 
+  bind_rows(data_2016) %>% 
+  select(!where(~all(is.na(.x))))
 
-intersect(what_names,is_even_names)
